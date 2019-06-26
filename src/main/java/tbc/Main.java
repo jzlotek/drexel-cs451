48dc9f3db5aa0dbfe0501f4358c898b984a27d3f
John Zlotek <jzlotek@gmail.com>
2019-06-26 11:07:03 -0400

    java

---
+++ src/main/java/tbc/Main.java
@@ -0,0 +1,31 @@
+package tbc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
