d6f6647c986aff5cd4a00cae887bad86cb492936
DESKTOP-7BTGJ2T\Horger <mh3294@drexel.edu>
2019-07-01 22:15:24 -0400

    Add Threaded Player Object to Client
    Removing server / socket folder - maintained functionality for V1 push

--- Client/src/main/java/tbc/Config.java
+++
@@ -1,6 +0,0 @@
-package tbc;
-
-public class Config {
-    public static final int PORT = 3210;
-    public static final String HOST = "127.0.0.1";
-}
---
+++ Client/src/main/java/tbc/client/components/Player.java
@@ -0,0 +1,47 @@
+package server.service;
+
+import java.io.*;
+import java.net.*;
+
+public class Player extends Thread { // each player will be it's own thread
+    
+	protected static Socket socket;
+	public static final int PORT = 451; // Using CS 451 as the port number
+    public static final String HOST = "10.246.251.14"; 	// Using Tux4 to host this service. DNS Map has us connecting to 10.246.251.14
+
+    /*
+     * Default Constructor
+     */
+    public Player(Socket clientSocket) {
+        this.socket = clientSocket;
+    }
+
+    
+    /*
+     * Getter for Socket Information
+     */
+    public Socket getSocket(){
+    	return this.socket;
+    }
+    
+    /*
+     * Setter to close the socket
+     */
+    public void setSocket(boolean isActive) {
+    	if(isActive) {
+    		// if we need to active the socket
+    	} else {
+    		// else close the socket
+    	}
+    }
+    
+    
+    public static void init() throws IOException{
+    	 try {
+             socket = new Socket(HOST, PORT);
+             System.out.println("Connected: " + socket.toString());
+         } catch (IOException e) {
+             e.printStackTrace();
+         }    	
+    }
+}
