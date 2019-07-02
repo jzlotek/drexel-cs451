81f232d9d2e858bb60b94c1c7647542b39f0b456
John Zlotek <jzlotek@gmail.com>
2019-07-02 12:58:25 -0400

    fixed

---
+++ src/main/java/tbc/server/ConsoleWrapper.java
@@ -0,0 +1,42 @@
+package tbc.server;
+
+import java.util.Scanner;
+
+/*
+ * Wrapper for Console I/O
+ */
+public class ConsoleWrapper {
+    public ConsoleWrapper() {
+
+    }
+
+    public static void Write(Object message) {
+        System.out.print(message);
+    }
+
+    public static void WriteLn(Object message) {
+        System.out.println(message);
+    }
+
+    public static String Read() {
+        try {
+            Scanner input = new Scanner(System.in);
+            while (input.hasNextLine()) {
+                return input.nextLine();
+            }
+        } catch (Exception ex) {
+            System.out.println("Console Input Invalid");
+        }
+        return "";
+    }
+
+    public static boolean CheckRead() {
+        Scanner input = new Scanner(System.in);
+        if (input.hasNext()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
