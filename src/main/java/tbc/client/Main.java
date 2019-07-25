2d13db9f485f79dab73fe5476f2e52b224a585f5
John Zlotek <jzlotek@gmail.com>
2019-07-25 16:20:06 -0400

    swing

--- src/main/java/tbc/client/Main.java
+++ src/main/java/tbc/client/Main.java
@@ -1,24 +1,61 @@
 package tbc.client;
 
-import javafx.application.Application;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import tbc.client.components.Piece;
-import tbc.client.scene.GameScene;
+import tbc.Constants;
+import tbc.client.components.Board;
+import tbc.util.ConsoleWrapper;
+import tbc.util.SerializationUtilJSON;
+import tbc.util.SocketUtil;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
 
 
-public class Main extends Application {
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        JFrame window = new JFrame();
+        JButton b = new JButton("click");
+        b.setBounds(130, 100, 100, 40);
+
+        window.add(b);
+        window.setSize(600, 600);
+        window.setLayout(null);
+        window.setVisible(true);
     }
 
-    @Override
-    public void start(Stage stage) {
-        GameScene gs = new GameScene(stage);
-        gs.addDrawableObject(new Piece(1,1, 10, 10, Color.BLACK));
-        gs.show();
-        gs.render();
+    public void gameLoop() {
+        Socket s = null;
+        SerializationUtilJSON serializer = new SerializationUtilJSON();
+        try {
+            s = new Socket("localhost", Constants.PORT);
+        } catch (Exception e) {
+            ConsoleWrapper.Write(e);
+        }
 
-    }
+        if (s != null) {
+            ConsoleWrapper.Write(s.toString());
+        }
+        // Game Loop
+        Board boardState = null;
+        BufferedReader socketReader = null;
+        try {
+            socketReader = SocketUtil.getReaderFromSocket(s);
+        } catch (Exception e) {
+            // throw exception and notify main javafx
+            ConsoleWrapper.WriteLn("Failed to get reader from socket");
+        }
+        while (true) {
+            String serverBoard = null;
+            while (serverBoard == null) {
+                try {
+                    serverBoard = socketReader.readLine();
+                } catch (IOException e) {
+                    serverBoard = null;
+                    continue;
+                }
+                ConsoleWrapper.WriteLn(serverBoard);
+            }
+//            boardState = serializer.deserialize(serverBoard, Board.class);
+        }
 }
