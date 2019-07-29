c251d8c743691a428ea96e13686a42b5f4a59f04
jsc334 <48026683+jsc334@users.noreply.github.com>
2019-07-29 17:47:15 -0400

    Checkers backbone setup (#18)
    * circleci release job
    
    * Update design.tex
    
    * Add some architecture design
    
    * Adding remaining content from drive
    
    * Updating for group assignments
    
    * swing
    
    * updated readme.md
    
    * Added some backbone for game implementation
    
    * linux
    
    * Added code to move pieces on board when a valid move is given
    
    * fix circleci AGAIN
    
    * fix main

--- src/main/java/tbc/client/Main.java
+++ src/main/java/tbc/client/Main.java
@@ -1,16 +1,13 @@
 package tbc.client;
 
 import tbc.Constants;
-import tbc.client.components.Board;
 import tbc.client.components.GameScene;
 import tbc.util.ConsoleWrapper;
 import tbc.util.SerializationUtilJSON;
-import tbc.util.SocketUtil;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -19,6 +16,23 @@ public class Main {
     static Socket s = null;
 
     public static void main(String[] args) {
+        JFrame window = new JFrame();
+        JButton b = new JButton("click");
+        b.setBounds(130, 100, 100, 40);
+
+        window.add(b);
+        window.setSize(600, 600);
+        window.setLayout(null);
+        window.setVisible(true);
+    }
+
+    public void gameLoop() {
+        SerializationUtilJSON serializer = new SerializationUtilJSON();
+        try {
+            s = new Socket("localhost", Constants.PORT);
+        } catch (Exception e) {
+            ConsoleWrapper.Write(e);
+        }
         GameScene scene = new GameScene();
         JTextArea debug = new JTextArea();
         debug.setBounds(400, 0, 200, 100);
@@ -44,7 +58,7 @@ public class Main {
                     if (s == null) {
                         debug.setText("Failed to Join");
                     } else {
-                        debug.setText(debug.getText()+"\nConnected to: " + s.getInetAddress().getHostAddress());
+                        debug.setText(debug.getText() + "\nConnected to: " + s.getInetAddress().getHostAddress());
                     }
                 }
                 ConsoleWrapper.WriteLn(e.toString());
@@ -56,37 +70,5 @@ public class Main {
         scene.add(joinButton);
         scene.add(debug);
         scene.show();
-    }
-
-    public void gameLoop() throws InterruptedException {
-        Socket s = null;
-        SerializationUtilJSON serializer = new SerializationUtilJSON();
-        try {
-            s = new Socket("localhost", Constants.PORT);
-        } catch (Exception e) {
-            ConsoleWrapper.Write(e);
-        }
-
-        if (s != null) {
-            ConsoleWrapper.Write(s.toString());
-        }
-        // Game Loop
-        Board boardState = null;
-        BufferedReader socketReader = null;
-        while (true) {
-            String serverBoard = null;
-            while (serverBoard == null) {
-                try {
-                    serverBoard = SocketUtil.readFromSocket(s);
-                } catch (IOException e) {
-                    serverBoard = null;
-                    Thread.sleep(500);
-                    ConsoleWrapper.WriteLn("Waiting 500 ms");
-                    continue;
-                }
-                ConsoleWrapper.WriteLn(serverBoard);
-            }
-//            boardState = serializer.deserialize(serverBoard, Board.class);
-        }
     }
 }
---
+++ src/main/java/tbc/client/checkers/Space.java
@@ -0,0 +1,45 @@
+package tbc.client.checkers;
+
+public class Space
+{
+	private Vector pos;
+	private Piece piece;
+	private Color color;
+	
+	public Space(Vector _pos, Color _color)
+	{
+		pos = _pos;
+		color = _color;
+	}
+	
+	public Space(int _x, int _y, Color _color)
+	{
+		pos = new Vector(_x, _y);
+		color = _color;
+	}
+	
+	public Vector getPos()
+	{
+		return pos;
+	}
+	
+	public boolean isOccupied()
+	{
+		return piece != null;
+	}
+	
+	public Piece getPiece()
+	{
+		return piece;
+	}
+	
+	public void setPiece(Piece _piece)
+	{
+		piece = _piece;
+	}
+	
+	public Color getColor()
+	{
+		return color;
+	}
+}
