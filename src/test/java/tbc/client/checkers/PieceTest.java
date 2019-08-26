6f6fe0fdfbe207ac52fe09d6cd2c445007a0ae09
Preet <me.preetpatel@gmail.com>
2019-08-26 17:13:51 -0400

    Even test cases private methods not covered

--- src/test/java/tbc/client/checkers/PieceTest.java
+++ src/test/java/tbc/client/checkers/PieceTest.java
@@ -1,62 +1,84 @@
 package tbc.client.checkers;
 
+import junit.framework.TestCase;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+public class PieceTest extends TestCase {
 
-import static org.junit.Assert.*;
-
-
-public class PieceTest {
-
-    @Test
-    public void getBoard() {
+    public void testGetBoard() {
         Board b = new Board();
-        Piece p1 = new Piece(b, Color.BLACK);
-        Piece p2 = new Piece(b, Color.RED);
-
-        assertEquals(p1.getBoard(),b);
-        assertEquals(p2.getBoard(),b);
+        Piece p1 = new Piece(b,Color.WHITE, new Vector(0,0));
+        assertEquals(p1.getBoard(), b);
+        Piece p2 = new Piece(b,Color.RED, new Vector(7,7));
+        assertEquals(p2.getBoard(), b);
     }
 
-    @Test
-    public void getColor() {
+    public void testGetColor() {
         Board b = new Board();
-        Piece p1 = new Piece(b,Color.BLACK);
-        Piece p2= new Piece(b,Color.RED);
-
-        assertEquals(p1.getColor(), Color.BLACK);
+        Piece p1 = new Piece(b,Color.WHITE, new Vector(0,0));
+        assertEquals(p1.getColor(), Color.WHITE);
+        Piece p2 = new Piece(b,Color.RED, new Vector(7,7));
         assertEquals(p2.getColor(), Color.RED);
     }
 
-    @Test
-    public void getIsAlive() {
+    public void testGetHasCrown() {
         Board b = new Board();
-        Piece p1 = new Piece(b, Color.BLACK);
-        assertTrue(p1.getIsAlive());
+        Piece p1 = new Piece(b,Color.WHITE, new Vector(0,0));
+        assertEquals(p1.getHasCrown(), false);
+
+        Piece p2 = new Piece(b,Color.RED, new Vector(7,7));
+        assertEquals(p2.getHasCrown(), false);
     }
 
-    @Test
-    public void setIsAlive() {
+    public void testSetHasCrown() {
         Board b = new Board();
-        Piece p1 = new Piece(b, Color.BLACK);
-        p1.setIsAlive(false);
-        assertFalse(p1.getIsAlive());
-
-    }
-
-    @Test
-    public void getHasCrown() {
-        Board b = new Board();
-        Piece p1 = new Piece(b, Color.BLACK);
-        assertFalse(p1.getHasCrown());
-    }
-
-    @Test
-    public void setHasCrown() {
-        Board b = new Board();
-        Piece p1 = new Piece(b, Color.BLACK);
+        Piece p1 = new Piece(b,Color.WHITE, new Vector(0,0));
+        assertEquals(p1.getHasCrown(), false);
         p1.setHasCrown(true);
-        assertTrue(p1.getHasCrown());
+        assertEquals(p1.getHasCrown(), true);
+
+        Piece p2 = new Piece(b,Color.RED, new Vector(7,7));
+        assertEquals(p2.getHasCrown(), false);
+        p2.setHasCrown(true);
+        assertEquals(p2.getHasCrown(), true);
+
     }
+
+    public void testGetUUID() {
+        Board b = new Board();
+        Piece p1 = new Piece(b,Color.WHITE, new Vector(0,0));
+
+        assertEquals(p1.getUUID().getClass(), b.getPiece(0,0).getUUID().getClass());
+
+    }
+
+    public void testGetPos() {
+        Board b = new Board();
+        Piece p1 = new Piece(b,Color.WHITE, new Vector(0,0));
+        assertEquals(p1.getPos().getX(), 0);
+    }
+
+    public void testSetPos() {
+        Board b = new Board();
+        Piece p1 = new Piece(b,Color.WHITE, new Vector(0,0));
+        assertEquals(p1.getPos().getX(), 0);
+
+        p1.setPos(new Vector(2,0));
+        assertEquals(p1.getPos().getX(), 2);
+
+    }
+
+
+    public void testGetOnPieceKilledHandler() {
+        Board b = new Board();
+        Piece p1 = new Piece(b,Color.WHITE, new Vector(0,0));
+        assertNotNull(p1.getOnPieceKilledHandler());
+
+    }
+
+    public void testGetOnPieceCrownedHandler() {
+        Board b = new Board();
+        Piece p1 = new Piece(b,Color.WHITE, new Vector(0,0));
+        assertNotNull(p1.getOnPieceCrownedHandler());
+    }
+
 }
